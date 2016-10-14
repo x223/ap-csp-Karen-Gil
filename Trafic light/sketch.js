f35d7ddddd0855add42b97a505599ef18a5cecf0
Karen-Gil <Karen.gil@ms223.org>
2016-10-14 14:28:05 -0400

    Add files via upload

---
+++ Trafic light/sketch.js
@@ -0,0 +1,21 @@
+var colors= ('white');
+function setup() {
+  
+}
+
+function draw() {
+  createCanvas(windowWidth, windowHeight);
+  text('black')
+  text(mouseX+ '-' + mouseY, 10,10);
+  fill('yellow');
+  stroke('yellow')
+  rect (40,30, 200,350, 60,50);
+//Lights
+fill('white')
+ellipse (140,100, 100,100);
+ellipse (140,200, 100,100)
+  ellipse (140,300, 100,100)
+}
+  If (mouseX < 2 *windowWidth / 3) {
+    colors = 
+}
